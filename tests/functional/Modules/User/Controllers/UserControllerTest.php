e6351bf3c1ddde25a42abd01296f2722d76ccb1a
ottowayne <mr.ottowayne@gmail.com>
2015-04-16 19:15:55 +0200

    [Tests] more tests

--- tests/functional/Modules/User/Controllers/UserControllerTest.php
+++ tests/functional/Modules/User/Controllers/UserControllerTest.php
@@ -1,8 +1,10 @@
 <?php namespace Tests\Functional\Modules\User\Controllers\Auth;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 use Modules\User\Entities\User;
 use Tests\TestCase;
+use Tests\Traits\UserTrait;
 
 /**
  * Created by PhpStorm.
@@ -13,14 +15,35 @@ use Tests\TestCase;
 class UserControllerTest extends TestCase
 {
 
-    // ToDo
+    use UserTrait;
 
     /**
      * @test
+     * @covers Modules\User\Http\Controllers\UserController::show
      */
-    public function it_is_true()
+    public function it_shows_a_user()
     {
-        $this->assertTrue(true);
+        $this->be($user = $this->createUser());
+
+        $this->visit(URL::route('user.profile', $user->id))
+            ->see($user->name);
     }
+
+    /**
+     * @test
+     * @covers Modules\User\Http\Controllers\UserController::edit
+     * @covers Modules\User\Http\Controllers\UserController::update
+     */
+    public function it_edits_a_user()
+    {
+        $this->be($user = $this->createUser());
+
+        $this->visit(URL::route('user.profile.edit'))
+            ->submitForm('Speichern', ['city' => 'footown'])
+            ->seeInDatabase('users', ['email' => $user->email, 'city' => 'footown'])
+            ->onPage(URL::route('user.profile', $user->id));
+    }
+
+    // ToDo: verify validation errors
 
 }
